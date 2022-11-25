e6ee4b0fce0b6823f54d63ab1db0f89b1b461eb6
hectorrojasch <rojasmery4@protonmail.com>
2022-11-24 22:52:10 -0300

    creación Brands db:seed 1

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 
 puts "Creating brands..."
 
-#article = Article.create
+
 
 brands = Brand.create([{ name: "Soprole" }, { name: "Lactosur" }, { name: "Colún" }, { name: "Calán" }, { name: "Parmalat" }, { name: "Nestlé" }, { name: "Ula" }, { name: "Danone" }])
 
