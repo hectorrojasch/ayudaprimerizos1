b9a824b14373628a8a1923978ecf0825a8353c7c
hectorrojasch <rojasmery4@protonmail.com>
2022-11-25 22:58:20 -0300

    creación de roles

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+
+	protected
+
+
+	def tengo_permisos?
+		
+		if !current_user.admin?
+			redirect_to articles_path, alert: "No tienes permisos para esto"
+		end
+	end	
+
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  enum role: [:common, :sailsman, :admin]
+
   has_many :articles, dependent: :destroy
+
+  after_initialize do
+    if self.new_record?
+       self.role ||= :common
+    end
+  
+  end
+
 end
